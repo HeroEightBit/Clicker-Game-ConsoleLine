33856e93c3b2c4bdcbca5efb931986cb1fede485
aaron.schultz <aaron.schultz@DT-9QX2793>
2021-10-01 09:09:18 -0400

    Add project files.

---
+++ ConsoleLineTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleLineTest
+{
+    class ClickerFactory
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+            var points = 0;
+            var totalPoints = 0;
+            var manualClick = 1;
+            var manualUpgradeCost = 10;
+            var manualUpgradeCostMulitplier = 1.1;
+
+
+            //Game Opening
+            Console.WriteLine("Begin Clicking!");
+
+            //Game
+            do
+            {
+
+                while (Console.KeyAvailable == false)
+                    Thread.Sleep(100); // Loop until input is entered.
+                cki = Console.ReadKey(true);
+
+                if (cki.Key == ConsoleKey.Spacebar)
+                {
+                    points += manualClick;
+                    totalPoints += manualClick;
+                }
+
+                if (cki.Key == ConsoleKey.U && points >= manualUpgradeCost)
+                {
+                    points -= manualUpgradeCost;
+                    manualClick += 1;
+                    manualUpgradeCost = Convert.ToInt32(Math.Ceiling(manualUpgradeCost * manualUpgradeCostMulitplier));
+                }
+       
+                Console.WriteLine($"Points: {points} Total Points: {totalPoints} Points Per Click: {manualClick} Upgrade Cost: {manualUpgradeCost}");
+            } while (cki.Key != ConsoleKey.Q);
+
+        }
+    }
+}
