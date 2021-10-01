192754e73547ef43eaed73b527e8bbf74e6a9125
HeroEightBit <aaronschultz1998@gmail.com>
2021-10-01 14:42:58 -0400

    added upgraded menu and points are stationary

--- ClickerGameConsoleLine/Program.cs
+++ ClickerGameConsoleLine/Program.cs
@@ -21,25 +21,27 @@ namespace ClickerGameConsoleLine
             var autoUpgradeCost = 50;
             var autoUpgradeCostMulitplier = 1.5;
 
-
             //Game Opening
             Console.WriteLine("Begin Clicking!");
+            Console.WriteLine("Press SPACEBAR to click!");
+            Console.WriteLine("Press U for the upgrade menu.");
 
             //Game
             do
             {
-
                 while (Console.KeyAvailable == false)
                 {
                     //Auto Clicker
                     if (autoClick > 0)
                     {
+                        Console.Clear();
                         points += automaticAdder(time, autoClick);
                         totalPoints += automaticAdder(time, autoClick);
-                        Console.WriteLine($"Points: {points} Total Points: {totalPoints} Points Per Click: {manualClick} Upgrade Cost: {manualUpgradeCost}");
+                        Console.WriteLine($"Points: {points} Total Points: {totalPoints} Points Per Click: {manualClick}");
                     }
                 }
                 Thread.Sleep(100); // Loop until input is entered.
+                Console.Clear();
                 cki = Console.ReadKey(true);
                 //Manual Clicker
                 if (cki.Key == ConsoleKey.Spacebar)
@@ -47,19 +49,37 @@ namespace ClickerGameConsoleLine
                     points += manualClick;
                     totalPoints += manualClick;
                 }
-                //Upgrade
-                if (cki.Key == ConsoleKey.U && points >= manualUpgradeCost)
+                //Upgrade EM
+                if (cki.Key == ConsoleKey.U)
                 {
-                    points -= manualUpgradeCost;
-                    manualClick += 1;
-                    manualUpgradeCost = Convert.ToInt32(Math.Ceiling(manualUpgradeCost * manualUpgradeCostMulitplier));
+                    Console.WriteLine("Welcome to the Upgrade Menu!");
+                    Console.WriteLine($"Current Points: {points}");
+                    Console.WriteLine($"A - Auto Clicker Upgrade Price {autoUpgradeCost}");
+                    Console.WriteLine($"M - Manual Clicker Upgrade Price {manualUpgradeCost}");
+                    
+                    cki = Console.ReadKey(true);
+                    if (cki.Key == ConsoleKey.A && points >= autoUpgradeCost)
+                    {
+                        points -= autoUpgradeCost;
+                        autoClick += 1;
+                        autoUpgradeCost = Convert.ToInt32(Math.Ceiling(autoUpgradeCost * autoUpgradeCostMulitplier));
+                    }
+                    if (cki.Key == ConsoleKey.M && points >= manualUpgradeCost)
+                    {
+                        points -= manualUpgradeCost;
+                        manualClick += 1;
+                        manualUpgradeCost = Convert.ToInt32(Math.Ceiling(manualUpgradeCost * manualUpgradeCostMulitplier));
+                    }
+                    if ((cki.Key == ConsoleKey.A && points < autoUpgradeCost)|| (cki.Key == ConsoleKey.M && points < manualUpgradeCost))
+                    {
+                        Console.WriteLine("Sorry, not enough points!");
+                    }
                 }
                 //Display
-                Console.WriteLine($"Points: {points} Total Points: {totalPoints} Points Per Click: {manualClick} Upgrade Cost: {manualUpgradeCost}");
+                Console.WriteLine($"Points: {points} Total Points: {totalPoints} Points Per Click: {manualClick}");
             } while (cki.Key != ConsoleKey.Q);
 
         }
-
         private static int automaticAdder(int time, int autoClick)
         {  
             Thread.Sleep(time); // Loop until input is entered.
